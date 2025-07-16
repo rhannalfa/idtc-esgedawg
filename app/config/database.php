e678abda9e8fcf4307a8b876a3ddafc248407f4f
rhannalfa <raihanalf9c@gmail.com>
2025-07-16 21:09:01 +0700

    update env database

--- app/config/database.php
+++ app/config/database.php
@@ -6,10 +6,10 @@ require_once __DIR__ . '/app.php';
 
 // Konfigurasi koneksi database Anda menggunakan variabel dari .env
 return [
-    'host' => getenv('DB_HOST') ?: 'localhost',
-    'dbname' => getenv('DB_NAME') ?: 'db_idtc',
-    'username' => getenv('DB_USER') ?: 'root',
-    'password' => getenv('DB_PASS') ?: '',
+    'host' => getenv('DB_HOST'),
+    'dbname' => getenv('DB_DATABASE'),
+    'username' => getenv('DB_USERNAME'),
+    'password' => getenv('DB_PASSWORD'),
     'charset' => 'utf8mb4',
     'options' => [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
